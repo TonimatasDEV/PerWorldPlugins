08869d6b7a58b6935ca6c2642165314b3e521861
TonimatasMC <amatascubero@gmail.com>
2022-08-20 05:40:59 +0200

    Basic compatibility [Origins-Bukkit]

--- src/main/java/net/tonimatasmc/perworldplugins/injector/listener/fixers/OriginsFixer.java
+++ src/main/java/net/tonimatasmc/perworldplugins/injector/listener/fixers/OriginsFixer.java
@@ -1,7 +1,11 @@
 package net.tonimatasmc.perworldplugins.injector.listener.fixers;
 
+import me.lemonypancakes.bukkit.origins.OriginsBukkit;
+import me.lemonypancakes.bukkit.origins.entity.player.OriginPlayer;
 import net.tonimatasmc.perworldplugins.PerWorldPlugins;
 import net.tonimatasmc.perworldplugins.storage.YML.Fixer;
+import net.tonimatasmc.perworldplugins.util.WorldUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,9 +19,13 @@ public class OriginsFixer implements Listener {
         if (event.getFrom() != event.getPlayer().getWorld()) {
             if (PerWorldPlugins.getPlugin().getServer().getPluginManager().getPlugin("Origins-Bukkit") != null) {
                 if (Fixer.getFix().getBoolean("OriginsFixer.enabled")) {
-                    //Object object = Fixer.getFix().get("OriginsFixer.origin");
-                    //OriginPlayer originPlayer = OriginsBukkit.getOriginPlayer(event.getPlayer());
-                    //((OriginLayer) object).setEnabled(!PerWorldPlugins.getPlugin().getConfig().getStringList("plugins.Origins-Bukkit").contains(WorldUtil.getWorldFromEvent(event).getName()));
+
+                    if (PerWorldPlugins.getPlugin().getConfig().getStringList("plugins.Origins-Bukkit").contains(WorldUtil.getWorldFromEvent(event).getName())) {
+                        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "origin set " + player.getName() + " origins-bukkit:origin origins-bukkit:human");
+                    } else {
+
+                        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "origin item give " + player.getName() + " origins-bukkit:orb_of_origin 1");
+                    }
                 }
             }
         }
