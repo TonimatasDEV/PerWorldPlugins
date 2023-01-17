99ca94e05ee66be77061b8ecf318548a6b054be9
TonimatasDEV <amatascubero@gmail.com>
2023-01-17 23:11:58 +0100

    Delete unnecessary code

--- src/main/java/net/tonimatasdev/perworldplugins/event/EnablePluginConfigCreator.java
+++ src/main/java/net/tonimatasdev/perworldplugins/event/EnablePluginConfigCreator.java
@@ -6,13 +6,12 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.server.PluginEnableEvent;
 
 import java.util.Collections;
-import java.util.Objects;
 
 public class EnablePluginConfigCreator implements Listener {
 
     @EventHandler
     public void onEnablePlugin(PluginEnableEvent event) {
-        if (!Objects.requireNonNull(PerWorldPlugins.getInstance().getConfig().getConfigurationSection("plugins")).contains(event.getPlugin().getName())) {
+        if (!PerWorldPlugins.getInstance().getConfig().getConfigurationSection("plugins").contains(event.getPlugin().getName())) {
             if (!event.getPlugin().getName().equalsIgnoreCase(PerWorldPlugins.getInstance().getName())) {
                 PerWorldPlugins.getInstance().getConfig().set("plugins." + event.getPlugin().getName(), Collections.singletonList("Example"));
                 PerWorldPlugins.getInstance().saveConfig();
