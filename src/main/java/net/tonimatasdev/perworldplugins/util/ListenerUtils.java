2cd5047e82e295024ce094806d86d8ea5b4a0f8e
TonimatasDEV <amatascubero@gmail.com>
2023-01-17 23:39:48 +0100

    Fix all

--- src/main/java/net/tonimatasdev/perworldplugins/util/ListenerUtils.java
+++ src/main/java/net/tonimatasdev/perworldplugins/util/ListenerUtils.java
@@ -4,6 +4,7 @@ import net.tonimatasdev.perworldplugins.PerWorldPlugins;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.Event;
+import org.bukkit.event.EventException;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
@@ -18,7 +19,7 @@ public class ListenerUtils {
         HandlerList.getHandlerLists().forEach((handlerList -> map.put(handlerList, handlerList.getRegisteredListeners())));
 
         for (Plugin plugin : Bukkit.getServer().getPluginManager().getPlugins()) {
-            if (!plugin.getName().equals("PerWorldPlugins") || !plugin.getName().equals("Geyser-Spigot") || !plugin.getName().equals("floodgate")) {
+            if (!plugin.getName().equals("PerWorldPlugins") && !plugin.getName().equals("Geyser-Spigot") && !plugin.getName().equals("floodgate")) {
                 HandlerList.unregisterAll(plugin);
             }
         }
@@ -28,20 +29,22 @@ public class ListenerUtils {
         for (RegisteredListener registeredListener : map.get(event.getHandlers())) {
             Plugin plugin = registeredListener.getPlugin();
 
-            if (!plugin.getName().equals("PerWorldPlugins")) {
+            if (!plugin.getName().equals("PerWorldPlugins") && !plugin.getName().equals("Geyser-Spigot") && !plugin.getName().equals("floodgate")) {
                 if (PerWorldPlugins.getInstance().getConfig().getBoolean("blacklist")) {
-                    if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(world.getName())) {
-                        event.getHandlers().unregister(registeredListener);
-                    } else {
-                        event.getHandlers().unregister(registeredListener);
-                        event.getHandlers().register(registeredListener);
+                    if (!PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(world.getName())) {
+                        try {
+                            registeredListener.callEvent(event);
+                        } catch (EventException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 } else {
                     if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(world.getName())) {
-                        event.getHandlers().unregister(registeredListener);
-                        event.getHandlers().register(registeredListener);
-                    } else {
-                        event.getHandlers().unregister(registeredListener);
+                        try {
+                            registeredListener.callEvent(event);
+                        } catch (EventException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
                 }
             }
