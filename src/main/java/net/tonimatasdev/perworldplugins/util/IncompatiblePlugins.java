d1ee6db74b4a2b804b63374ca4ca0bac1eeae58d
TonimatasDEV <amatascubero@gmail.com>
2023-01-18 16:30:31 +0100

    Delete unnecessary code

--- src/main/java/net/tonimatasdev/perworldplugins/util/IncompatiblePlugins.java
+++ src/main/java/net/tonimatasdev/perworldplugins/util/IncompatiblePlugins.java
@@ -6,43 +6,43 @@ import org.bukkit.plugin.Plugin;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 
 //Some plugins don't add their commands to plugin.yml and this class detects that the plugin has a command in text.
 public class IncompatiblePlugins {
+    @SuppressWarnings("DataFlowIssue")
     public static Plugin getIncompatiblePluginWithCommand(String command) {
-        if (Objects.requireNonNull(getList(0)).contains(command)) {
+        if (getList(0).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("BetterTeams").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("BetterTeams");
             } else {
                 return null;
             }
-        } else if (Objects.requireNonNull(getList(1)).contains(command)) {
+        } else if (getList(1).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("ExcellentCrates").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("ExcellentCrates");
             } else {
                 return null;
             }
-        } else if (Objects.requireNonNull(getList(2)).contains(command)) {
+        } else if (getList(2).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("NextEngine").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("NextEngine");
             } else {
                 return null;
             }
-        } else if (Objects.requireNonNull(getList(3)).contains(command)) {
+        } else if (getList(3).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("MoneyHunters").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("MoneyHunters");
             } else {
                 return null;
             }
-        } else if (Objects.requireNonNull(getList(4)).contains(command)) {
+        } else if (getList(4).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("Origins-Bukkit").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("Origins-Bukkit");
             } else {
                 return null;
             }
-        } else if (Objects.requireNonNull(getList(5)).contains(command)) {
+        } else if (getList(5).contains(command)) {
             if (Bukkit.getPluginManager().getPlugin("EconomyShopGUI").isEnabled()) {
                 return Bukkit.getPluginManager().getPlugin("EconomyShopGUI");
             } else {
