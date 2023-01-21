e0eee545754b884fc2f1fe930d28655af0879668
TonimatasDEV <amatascubero@gmail.com>
2023-01-21 14:23:48 +0100

    Temporal delete command checker fix

--- src/main/java/net/tonimatasdev/perworldplugins/event/CommandPreProcessListener.java
+++ src/main/java/net/tonimatasdev/perworldplugins/event/CommandPreProcessListener.java
@@ -1,10 +1,8 @@
 package net.tonimatasdev.perworldplugins.event;
 
 import net.tonimatasdev.perworldplugins.PerWorldPlugins;
-import net.tonimatasdev.perworldplugins.util.IncompatiblePlugins;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -14,23 +12,12 @@ import org.bukkit.plugin.Plugin;
 public class CommandPreProcessListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
-        String command = event.getMessage().split(" ", 2)[0].replace("/", "");
-        PluginCommand pluginCommand = Bukkit.getPluginCommand(command);
-        String worldName = event.getPlayer().getWorld().getName();
+        Plugin plugin = Bukkit.getPluginCommand(event.getMessage().split(" ", 2)[0].replace("/", "")).getPlugin();
 
-        if (pluginCommand != null) {
-            if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + pluginCommand.getPlugin().getName()).contains(worldName)) {
+        if (plugin != null) {
+            if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(event.getPlayer().getWorld().getName())) {
                 event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldPlugins.getInstance().getConfig().getString("disabledCommandMessage")));
                 event.setCancelled(true);
-            }
-        } else {
-            Plugin plugin = IncompatiblePlugins.getIncompatiblePluginWithCommand(command);
-
-            if (plugin != null) {
-                if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(worldName)) {
-                    event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldPlugins.getInstance().getConfig().getString("disabledCommandMessage")));
-                    event.setCancelled(true);
-                }
             }
         }
     }
