692902affa69707ce65598316d76a5cfe0370e97
TonimatasDEV <amatascubero@gmail.com>
2023-03-09 16:10:49 +0100

    Optimize

--- src/main/java/net/tonimatasdev/perworldplugins/event/CommandPreProcessListener.java
+++ src/main/java/net/tonimatasdev/perworldplugins/event/CommandPreProcessListener.java
@@ -4,21 +4,32 @@ import net.tonimatasdev.perworldplugins.PerWorldPlugins;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import java.util.List;
+
 public class CommandPreProcessListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
-        PluginCommand pluginCommand = Bukkit.getPluginCommand(event.getMessage().split(" ", 2)[0].replace("/", ""));
+        Player player = event.getPlayer();
+        String commandName = event.getMessage().split(" ", 2)[0].replace("/", "");
 
-        if (pluginCommand != null) {
-            if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + pluginCommand.getPlugin().getName()).contains(event.getPlayer().getWorld().getName())) {
-                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldPlugins.getInstance().getConfig().getString("disabledCommandMessage")));
-                event.setCancelled(true);
-            }
+        PluginCommand pluginCommand = Bukkit.getPluginCommand(commandName);
+        if (pluginCommand == null) {
+            return;
+        }
+
+        String pluginName = pluginCommand.getPlugin().getName();
+        List<String> disabledWorlds = PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + pluginName);
+        String worldName = player.getWorld().getName();
+
+        if (disabledWorlds.contains(worldName)) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', PerWorldPlugins.getInstance().getConfig().getString("disabledCommandMessage")));
+            event.setCancelled(true);
         }
     }
 }
--- src/main/java/net/tonimatasdev/perworldplugins/util/ListenerUtils.java
+++ src/main/java/net/tonimatasdev/perworldplugins/util/ListenerUtils.java
@@ -4,16 +4,14 @@ import net.tonimatasdev.perworldplugins.PerWorldPlugins;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.configuration.Configuration;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ListenerUtils {
     private static final Map<HandlerList, RegisteredListener[]> map = new HashMap<>();
@@ -21,44 +19,51 @@ public class ListenerUtils {
 
     public static void addListeners() {
         HandlerListUtil.minecraftHandlerLists.forEach((handlerList -> {
-            map.put(handlerList, handlerList.getRegisteredListeners());
+            ArrayList<RegisteredListener> arrayList = new ArrayList<>();
 
-            for (RegisteredListener registeredListener : handlerList.getRegisteredListeners()) {
-                if (!plugins.contains(registeredListener.getPlugin().getName())) {
-                    handlerList.unregister(registeredListener);
-                }
-            }
+            Arrays.stream(handlerList.getRegisteredListeners())
+                    .filter(registeredListener -> !plugins.contains(registeredListener.getPlugin().getName()))
+                    .forEach(registeredListener -> {
+                        arrayList.add(registeredListener);
+                        handlerList.unregister(registeredListener);
+                    });
+
+            map.put(handlerList, arrayList.toArray(new RegisteredListener[arrayList.size()]));
         }));
 
         Bukkit.getConsoleSender().sendMessage("[PerWorldPlugins] " + ChatColor.GREEN + "Unregistered all Listeners correctly.");
     }
 
     public static void perWorldPlugins(Event event, World world) {
+        Configuration config = PerWorldPlugins.getInstance().getConfig();
+
         RegisteredListener[] registeredListeners = map.get(event.getHandlers());
+        if (registeredListeners == null) {
+            return;
+        }
 
-        if (registeredListeners != null) {
-            for (RegisteredListener registeredListener : registeredListeners) {
-                Plugin plugin = registeredListener.getPlugin();
+        for (RegisteredListener registeredListener : registeredListeners) {
+            Plugin plugin = registeredListener.getPlugin();
+            String pluginName = plugin.getName();
 
-                if (!plugins.contains(plugin.getName())) {
-                    if (PerWorldPlugins.getInstance().getConfig().getBoolean("blacklist")) {
-                        if (!PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(world.getName())) {
-                            try {
-                                registeredListener.callEvent(event);
-                            } catch (EventException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    } else {
-                        if (PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName()).contains(world.getName())) {
-                            try {
-                                registeredListener.callEvent(event);
-                            } catch (EventException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                    }
+            if (plugins.contains(pluginName)) {
+                continue;
+            }
 
+            boolean shouldCall;
+            List<String> worldList = config.getStringList("plugins." + pluginName);
+
+            if (config.getBoolean("blacklist")) {
+                shouldCall = !worldList.contains(world.getName());
+            } else {
+                shouldCall = worldList.contains(world.getName());
+            }
+
+            if (shouldCall) {
+                try {
+                    registeredListener.callEvent(event);
+                } catch (EventException e) {
+                    throw new RuntimeException(e);
                 }
             }
         }
