319d9362932c89f49d6f5c299a7849677d0003a9
TonimatasDEV <amatascubero@gmail.com>
2023-03-26 16:50:29 +0200

    Update command

--- src/main/java/net/tonimatasdev/perworldplugins/command/Command.java
+++ src/main/java/net/tonimatasdev/perworldplugins/command/Command.java
@@ -8,30 +8,41 @@ import org.bukkit.command.CommandSender;
 
 public class Command implements CommandExecutor {
 
-    @SuppressWarnings("NullableProblems")
     @Override
+    @SuppressWarnings("NullableProblems")
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("perworldplugins") && args.length == 1) {
             if (args[0].equalsIgnoreCase("version")) {
-                if (sender.hasPermission("perworldplugins.version")) {
-                    sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_GREEN + "+" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE + "The plugin version is: " + PerWorldPlugins.getInstance().getDescription().getVersion());
-                } else {
-                    sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_RED + "-" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE + "You don't have permissions for execute this command");
+                if (hasPermission(sender, "perworldplugins.version")) {
+                    sender.sendMessage(getPrefix(ChatColor.DARK_GREEN) + "The plugin version is: " + PerWorldPlugins.getInstance().getDescription().getVersion());
                 }
-            } else if (args[0].equalsIgnoreCase("reload")) {
-                if (sender.hasPermission("perworldplugins.reload")) {
+            }
+
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (hasPermission(sender, "perworldplugins.reload")) {
                     PerWorldPlugins.getInstance().reloadConfig();
                     PerWorldPlugins.getInstance().saveConfig();
                     GroupsYML.reload();
                     GroupsYML.save();
-                    sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_GREEN + "+" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE + "The plugin has been reloaded.");
-                } else {
-                    sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_RED + "-" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE + "You don't have permissions for execute this command");
+                    sender.sendMessage(getPrefix(ChatColor.DARK_GREEN) + "The plugin has been reloaded.");
                 }
             } else {
-                sender.sendMessage(ChatColor.WHITE + "[" + ChatColor.DARK_RED + "-" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE + "Please use: /perworldplugins reload | version");
+                sender.sendMessage(getPrefix(ChatColor.DARK_RED) + "Please use: /perworldplugins reload | version");
             }
         }
         return true;
+    }
+
+    private boolean hasPermission(CommandSender sender, String permission) {
+        if (sender.hasPermission(permission)) {
+            return true;
+        } else {
+            sender.sendMessage(getPrefix(ChatColor.DARK_RED) + "You don't have permissions for execute this command");
+            return false;
+        }
+    }
+
+    private String getPrefix(ChatColor chatColor) {
+        return ChatColor.WHITE + "[" + chatColor + "+" + ChatColor.WHITE + "] PerWorldPlugins: " + ChatColor.WHITE;
     }
 }
