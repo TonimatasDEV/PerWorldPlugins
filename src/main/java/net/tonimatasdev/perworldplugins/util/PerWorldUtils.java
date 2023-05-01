32fcf68d7b4972e8566f197ce738d65c60ef88c2
TonimatasDEV <amatascubero@gmail.com>
2023-05-01 19:21:13 +0200

    Fix get plugins problem

--- src/main/java/net/tonimatasdev/perworldplugins/util/PerWorldUtils.java
+++ src/main/java/net/tonimatasdev/perworldplugins/util/PerWorldUtils.java
@@ -1,7 +1,6 @@
 package net.tonimatasdev.perworldplugins.util;
 
 import net.tonimatasdev.perworldplugins.PerWorldPlugins;
-import net.tonimatasdev.perworldplugins.config.GroupsYML;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
@@ -10,20 +9,20 @@ import java.util.List;
 public class PerWorldUtils {
     public static boolean check(World world, Plugin plugin) {
         List<String> worldList = PerWorldPlugins.getInstance().getConfig().getStringList("plugins." + plugin.getName());
-        boolean isInPlugin = false;
+        boolean isInPlugin;
 
-        for (String var : worldList) {
-            if (var.contains(":")) {
-                isInPlugin = GroupsYML.get().getStringList(var.replaceAll("g:", "")).contains(world.getName());
-            }
-        }
+        //for (String var : worldList) {
+        //    if (var.contains(":")) {
+        //        isInPlugin = GroupsYML.get().getStringList(var.replaceAll("g:", "")).contains(world.getName());
+        //    }
+        //}
 
-        if (!isInPlugin) isInPlugin = worldList.contains(world.getName());
+        isInPlugin = !worldList.contains(world.getName());
 
         if (PerWorldPlugins.getInstance().getConfig().getBoolean("blacklist")) isInPlugin = !isInPlugin;
 
-        if (plugin.getName().equalsIgnoreCase("PerWorldPlugins")) isInPlugin = true;
+        if (plugin.getName().equalsIgnoreCase("PerWorldPlugins")) isInPlugin = false;
 
-        return !isInPlugin;
+        return isInPlugin;
     }
 }
