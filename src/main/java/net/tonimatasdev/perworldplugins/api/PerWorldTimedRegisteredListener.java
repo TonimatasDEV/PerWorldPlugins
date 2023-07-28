52cb9ea638a20caef97c77338c938ff9382fa973
TonimatasDEV <amatascubero@gmail.com>
2023-07-28 18:00:16 +0200

    TimedRegisteredListeners

---
+++ src/main/java/net/tonimatasdev/perworldplugins/api/PerWorldTimedRegisteredListener.java
@@ -0,0 +1,69 @@
+package net.tonimatasdev.perworldplugins.api;
+
+import net.tonimatasdev.perworldplugins.util.PerWorldUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockEvent;
+import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.hanging.HangingEvent;
+import org.bukkit.event.inventory.InventoryEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.vehicle.VehicleEvent;
+import org.bukkit.event.weather.WeatherEvent;
+import org.bukkit.event.world.WorldEvent;
+import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.TimedRegisteredListener;
+
+import java.util.logging.Level;
+
+public class PerWorldTimedRegisteredListener extends TimedRegisteredListener {
+    public PerWorldTimedRegisteredListener(Listener listener, EventExecutor executor, EventPriority priority, Plugin plugin, boolean ignoreCancelled) {
+        super(listener, executor, priority, plugin, ignoreCancelled);
+    }
+
+    @SuppressWarnings("NullableProblems")
+    public void callEvent(Event event) {
+        // If X-Events get player and detect if blocked so that it is not run.
+        if (event instanceof BlockEvent) {
+            if (PerWorldUtils.check(((BlockEvent) event).getBlock().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof EntityEvent) {
+            if (PerWorldUtils.check(((EntityEvent) event).getEntity().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof HangingEvent) {
+            if (PerWorldUtils.check(((HangingEvent) event).getEntity().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof InventoryEvent) {
+            if (PerWorldUtils.check(((InventoryEvent) event).getView().getPlayer().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof PlayerEvent) {
+            if (PerWorldUtils.check(((PlayerEvent) event).getPlayer().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof VehicleEvent) {
+            if (PerWorldUtils.check(((VehicleEvent) event).getVehicle().getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof WeatherEvent) {
+            if (PerWorldUtils.check(((WeatherEvent) event).getWorld(), getPlugin())) return;
+        }
+
+        if (event instanceof WorldEvent) {
+            if (PerWorldUtils.check(((WorldEvent) event).getWorld(), getPlugin())) return;
+        }
+
+        try {
+            // Execute the event if it is not blocked.
+            super.callEvent(event);
+        } catch (Throwable ex) {
+            Bukkit.getServer().getLogger().log(Level.SEVERE, "Could not pass event " + event.getEventName() + " to " + getPlugin().getName() + " v" + getPlugin().getDescription().getVersion(), ex);
+        }
+    }
+}
--- src/main/java/net/tonimatasdev/perworldplugins/manager/ListenerManager.java
+++ src/main/java/net/tonimatasdev/perworldplugins/manager/ListenerManager.java
@@ -1,10 +1,12 @@
 package net.tonimatasdev.perworldplugins.manager;
 
 import net.tonimatasdev.perworldplugins.api.PerWorldRegisteredListener;
+import net.tonimatasdev.perworldplugins.api.PerWorldTimedRegisteredListener;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
+import org.bukkit.plugin.TimedRegisteredListener;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -13,27 +15,41 @@ public class ListenerManager {
     public static void convert(Plugin plugin) {
         // Get all RegisteredListeners of the plugin and convert 1 per 1.
         for (RegisteredListener registeredListener : HandlerList.getRegisteredListeners(plugin)) {
-            if (registeredListener instanceof PerWorldRegisteredListener) continue; // If RegisteredListener is already converted it passes.
+            if (registeredListener instanceof PerWorldRegisteredListener)
+                continue; // If RegisteredListener is already converted it passes.
 
             // Get all HandlerList for get RegisteredListener HandlerList.
             for (HandlerList handlerList : HandlerList.getHandlerLists()) {
-                if (!Arrays.asList(handlerList.getRegisteredListeners()).contains(registeredListener)) continue; // See if the RegisteredListener is in this HandlerList or not.
+                if (!Arrays.asList(handlerList.getRegisteredListeners()).contains(registeredListener))
+                    continue; // See if the RegisteredListener is in this HandlerList or not.
 
                 // Unregister RegisteredListener.
                 handlerList.unregister(registeredListener);
 
-                try {
-                    // Get executor.
-                    Field executorField = RegisteredListener.class.getDeclaredField("executor");
-                    executorField.setAccessible(true);
+                if (registeredListener instanceof TimedRegisteredListener) {
+                    try {
+                        // Get executor.
+                        Field executorField = RegisteredListener.class.getDeclaredField("executor");
+                        executorField.setAccessible(true);
 
-                    // RegisteredListener to PerWorldRegisteredListener.
-                    handlerList.register(new PerWorldRegisteredListener(registeredListener.getListener(), (EventExecutor) executorField.get(registeredListener), registeredListener.getPriority(), registeredListener.getPlugin(), registeredListener.isIgnoringCancelled()));
-                } catch (NoSuchFieldException | IllegalAccessException e) {
-                    e.printStackTrace();
+                        // RegisteredListener to PerWorldRegisteredListener.
+                        handlerList.register(new PerWorldTimedRegisteredListener(registeredListener.getListener(), (EventExecutor) executorField.get(registeredListener), registeredListener.getPriority(), registeredListener.getPlugin(), registeredListener.isIgnoringCancelled()));
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        // Get executor.
+                        Field executorField = RegisteredListener.class.getDeclaredField("executor");
+                        executorField.setAccessible(true);
+
+                        // RegisteredListener to PerWorldRegisteredListener.
+                        handlerList.register(new PerWorldRegisteredListener(registeredListener.getListener(), (EventExecutor) executorField.get(registeredListener), registeredListener.getPriority(), registeredListener.getPlugin(), registeredListener.isIgnoringCancelled()));
+                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
-
     }
 }
